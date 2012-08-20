1da12988e6bd6815ee2cbb28441c13b2dc7e27e8
Christian Kuhn <lolli@schwarzbu.ch>
2012-08-20 16:04:19 +0200

    [TASK] Add phpunit fields to database

---
+++ dbimport/phpunit.sql
@@ -0,0 +1,2 @@
+ALTER table sys_category ADD tx_phpunit_is_dummy_record tinyint(1) unsigned DEFAULT '0' NOT NULL;
+ALTER table sys_category_record_mm ADD tx_phpunit_is_dummy_record tinyint(1) unsigned DEFAULT '0' NOT NULL;
