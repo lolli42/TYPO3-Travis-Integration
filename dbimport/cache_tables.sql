d062d8be4aff958baaa2c7f9dde8e5ad94d81a6d
Christian Kuhn <lolli@schwarzbu.ch>
2012-09-11 00:03:50 +0200

    [TASK] Add additional cache tables
    This adds new rootline caching framework tables to the environment.
    Affected are cf_cache_rootline, cf_extbase_datamapfactory_datamap
    and f_extbase_typo3dbbackend_tablecolumns

--- dbimport/cache_tables.sql
+++ dbimport/cache_tables.sql
@@ -16,6 +16,45 @@ CREATE TABLE cf_cache_hash_tags (
 	KEY cache_tag (tag)
 ) ENGINE=InnoDB;
 
+
+CREATE TABLE cf_cache_rootline (
+	id int(11) unsigned NOT NULL auto_increment,
+	identifier varchar(250) DEFAULT '' NOT NULL,
+	expires int(11) unsigned DEFAULT '0' NOT NULL,
+	content mediumblob,
+	PRIMARY KEY (id),
+	KEY cache_id (identifier,expires)
+) ENGINE=InnoDB;
+
+CREATE TABLE cf_cache_rootline_tags (
+	id int(11) unsigned NOT NULL auto_increment,
+	identifier varchar(250) DEFAULT '' NOT NULL,
+	tag varchar(250) DEFAULT '' NOT NULL,
+	PRIMARY KEY (id),
+	KEY cache_id (identifier),
+	KEY cache_tag (tag)
+) ENGINE=InnoDB;
+
+
+CREATE TABLE cf_extbase_datamapfactory_datamap (
+	id int(11) unsigned NOT NULL auto_increment,
+	identifier varchar(250) DEFAULT '' NOT NULL,
+	expires int(11) unsigned DEFAULT '0' NOT NULL,
+	content mediumblob,
+	PRIMARY KEY (id),
+	KEY cache_id (identifier,expires)
+) ENGINE=InnoDB;
+
+CREATE TABLE cf_extbase_datamapfactory_datamap_tags (
+	id int(11) unsigned NOT NULL auto_increment,
+	identifier varchar(250) DEFAULT '' NOT NULL,
+	tag varchar(250) DEFAULT '' NOT NULL,
+	PRIMARY KEY (id),
+	KEY cache_id (identifier),
+	KEY cache_tag (tag)
+) ENGINE=InnoDB;
+
+
 CREATE TABLE cf_extbase_object (
 	id int(11) unsigned NOT NULL auto_increment,
 	identifier varchar(250) DEFAULT '' NOT NULL,
@@ -26,6 +65,25 @@ CREATE TABLE cf_extbase_object (
 ) ENGINE=InnoDB;
 
 CREATE TABLE cf_extbase_object_tags (
+	id int(11) unsigned NOT NULL auto_increment,
+	identifier varchar(250) DEFAULT '' NOT NULL,
+	tag varchar(250) DEFAULT '' NOT NULL,
+	PRIMARY KEY (id),
+	KEY cache_id (identifier),
+	KEY cache_tag (tag)
+) ENGINE=InnoDB;
+
+
+CREATE TABLE cf_extbase_typo3dbbackend_tablecolumns (
+	id int(11) unsigned NOT NULL auto_increment,
+	identifier varchar(250) DEFAULT '' NOT NULL,
+	expires int(11) unsigned DEFAULT '0' NOT NULL,
+	content mediumblob,
+	PRIMARY KEY (id),
+	KEY cache_id (identifier,expires)
+) ENGINE=InnoDB;
+
+CREATE TABLE cf_extbase_typo3dbbackend_tablecolumns_tags (
 	id int(11) unsigned NOT NULL auto_increment,
 	identifier varchar(250) DEFAULT '' NOT NULL,
 	tag varchar(250) DEFAULT '' NOT NULL,
