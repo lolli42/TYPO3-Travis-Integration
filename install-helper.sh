e46864f38e7b66b7d3375061bbe241ac53f0a515
Helmut Hummel <helmut.hummel@typo3.org>
2014-02-10 20:18:56 +0100

    [TASK] Speedup lint by executing in parallel

--- install-helper.sh
+++ install-helper.sh
@@ -56,24 +56,11 @@ function installRedis() {
 }
 
 function phpLint {
-	for file in `find . -name \*.php`
-	do
-		echo -en "\r\033[KChecking: $file"
-		if ! php -l $file > /dev/null
-		then
-			echo -en "\r\033[K"
-			echo -e "\e[00;31mSyntax errors detected in file: $file\e[00m"
-			ERR=1
-			[ -z "$1" ] && break
-		fi
-	done
-
-	echo -e "\r\033[K"
-
-	if [ -n "$ERR" ]
+	if find . -name \*.php | parallel --gnu --keep-order 'php -l {}' > /tmp/errors
 	then
+		return 0
+	else
+		grep -v "No syntax errors detected in" /tmp/errors
 		return 99
 	fi
-
-	return 0
 }
