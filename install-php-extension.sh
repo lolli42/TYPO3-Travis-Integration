27d61a383b0b6fdf52042620233b8fe3a0a3e776
Helmut Hummel <helmut.hummel@typo3.org>
2014-02-11 12:28:17 +0100

    [TASK] Remove old not needed scripts

--- install-php-extension.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-name=$1
-pecl install $name
-[ -z "`grep $name.so $phpConfigFile`" ] && echo "extension=\"$name.so\"" >> $phpConfigFile
-
-exit 0
--- install-php-extensions.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-pecl install igbinary > /dev/null
-printf "no\n" | pecl install memcache > /dev/null
-echo "extension=\"memcache.so\"" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
-echo "extension=\"igbinary.so\"" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
